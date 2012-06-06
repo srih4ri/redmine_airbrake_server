8d317aee050c4941474ec6557261c2889f8090ae
srihari <srihari@mobizion.com>
2012-06-06 12:30:27 +0530

    Add routes for settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 RedmineApp::Application.routes.draw do
   post '/notifier_api/v2/notices/', :controller => 'airbrake', :action => 'index'
+post '/airbrake_server_project_settings',:controller =>'airbrake_server_project_settings',:action => 'update'
 end
